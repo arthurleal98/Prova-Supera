ea4c20db64c093f6638962b46224c56fa77d365a
arthurleal98 <arthurleal1@hotmail.com>
2021-10-04 03:13:34 -0300

    Adição do navBar

---
+++ src/navBar/navBar.js
@@ -0,0 +1,55 @@
+import './navBar.css';
+import { Link } from 'react-router-dom';
+import cart from '../assets/shopping-cart.png';
+const NavBar = (props) =>{
+    const styleNav={
+        display:'flex',
+        justifyContent:'space-between',
+        backgroundColor:'red',
+        height:'7rem',
+        alignItems:'center',
+        paddingLeft:'40px',
+        paddingRight:'60px'
+
+    }
+    const styleCart={
+        width:40,
+        height:40,
+        alignSelf: 'center'
+
+    }
+    const styleH4={
+        margin:0
+    }
+    const styleP={
+        margin:0,
+        float:'right'
+    }
+    const styleContentCart={
+        alignSelf: 'center'
+
+    }
+   
+    return(
+        <nav style={styleNav}>
+            <div id='Logo'>
+                <h1><Link to='/' className='link'>Supera Games</Link></h1>                
+            </div>               
+            <div id='nav-bar-cart'>
+                
+                <Link to='/cart' className='link'>
+                    <div id='nav-cart'>
+                        <img src={cart} alt='cart' style={styleCart}/>
+                        <div style={styleContentCart}>
+                            <h4 style={styleH4}>Carrinho</h4>
+                            <p style={styleP}>{props.qtdItems} produto</p>
+                        </div>
+                        
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+
+}
+export default NavBar;
