865ee9e67913f97186ce9b5a4b5ea5a2146062c0
arthurleal98 <arthurleal1@hotmail.com>
2021-10-06 21:16:07 -0300

    melhora no layout

--- src/home/home.js
+++ src/home/home.js
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
 
 const Home = () =>{
+    const styleHome={
+        textAlign:'center',
+        color: '#b8b6b4',
+        height:'100%',
+        marginTop:200
+    }
+
+    
     return(
-        <div className='container'>
-            <h1>Bem-vindo a Supera Games</h1>
-            <button className='btn btn-primary'><Link to='/games'/></button>
+        <div  style={styleHome}>
+            
+            <div >
+                <h1>Bem-vindo a Supera Games</h1>
+                <Link to='/games' className='link'><button className='btn btn-primary my-2 py-2'>Visitar Loja</button></Link>
+            </div>
+            
 
         </div>
     )
