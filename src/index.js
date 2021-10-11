f39a24789e3e7e6ff3799367b03a0f54aa82999b
arthurleal98 <42595872+arthurleal98@users.noreply.github.com>
2021-10-11 17:40:58 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import reactDom from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App.js';
 import './index.css';
 reactDom.render(
+    <React.StrictMode>
     <HashRouter>
         <App/>
-    </HashRouter>,document.getElementById('root')
+    </HashRouter>
+    </React.StrictMode>
+    
+    ,document.getElementById('root')
 );
